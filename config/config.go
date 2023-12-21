08b86e6f6d9c4db558e08237d6637c0b716ce2e7
Anya <Galickaya_ane4ka@bk.ru>
2023-12-21 13:45:41 +0300

    fixed http_server

--- config/config.go
+++ config/config.go
@@ -1,10 +1,8 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
 type Config struct {
@@ -20,10 +18,6 @@ func GetConfig() *Config {
 func getEnv(key string, defaultVal string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
-	}
-	for _, e := range os.Environ() {
-		pair := strings.SplitN(e, "=", 2)
-		fmt.Println(pair[0])
 	}
 
 	log.Print("No .env file found")
